22f3d7aae1dc46e1c42e4d30fa578797aff72dc9
Eliise S <elseling@microsoft.com>
2021-04-16 01:52:49 +0200

    Add iot device provisioning script

---
+++ scripts/terraform/register_iot_edge_device.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+create() {
+    az iot hub device-identity create --device-id "$IOT_EDGE_DEVICE_NAME" --edge-enabled --hub-name "$IOT_HUB_NAME" --output none
+    # shellcheck disable=SC2162
+    read
+}
+
+read() {
+    az iot hub device-identity connection-string show --device-id "$IOT_EDGE_DEVICE_NAME" --hub-name "$IOT_HUB_NAME"
+}
+
+delete() {
+    # TODO: Investigate issue sometimes updating (deleting then creating) create fails. Perhaps takes too long to delete?
+    az iot hub device-identity delete --device-id "$IOT_EDGE_DEVICE_NAME" --hub-name "$IOT_HUB_NAME"
+}
+
+# Check if the function exists (bash specific)
+if declare -f "$1" >/dev/null; then
+    # call arguments verbatim
+    "$@"
+else
+    # Show a helpful error
+    echo "'$1' is not a known function name" >&2
+    exit 1
+fi
